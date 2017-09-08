2f1424ac61bd2bd70258242609d765839f1f19a5
Mahdi Alouane <entijaaba2@gmail.com>
2017-09-08 17:26:18 +0100

    no blink

--- js/form.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.container .col-md-3 input').focus(function(){
-        $(this).parents('.col-md-3').addClass('flash');
-        setTimeout(function() {
-            $(this).parents('.col-md-3').addClass('flash');
-        }, 600);
-    });
-});
